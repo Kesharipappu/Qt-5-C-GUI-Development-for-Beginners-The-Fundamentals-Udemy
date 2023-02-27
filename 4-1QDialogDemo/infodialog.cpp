69e7e1ba384d14f4ccce9d7a32daadd7b5e812c6
PAPPU KUMAR KESHARI <92083274+Kesharipappu@users.noreply.github.com>
2023-02-27 14:57:25 +0530

    Add files via upload

---
+++ 4-1QDialogDemo/infodialog.cpp
@@ -0,0 +1,6 @@
+#include "infodialog.h"
+
+infodialog::infodialog()
+{
+
+}
---
+++ 4-1QDialogDemo/infodialog.h
@@ -0,0 +1,11 @@
+#ifndef INFODIALOG_H
+#define INFODIALOG_H
+
+
+class infodialog
+{
+public:
+    infodialog();
+};
+
+#endif // INFODIALOG_H
